c33f7a1bb554023d4fa2e0f46acd621d340fc97d
ikonon <315793076@qq.com>
2023-01-25 21:54:38 +0800

    chore: cli

--- test/animator.test.ts
+++ test/animator.test.ts
@@ -1,12 +1,10 @@
 import { describe, expect, it } from 'vitest'
-import { calculatePatches, createAnimator, diffString } from '../packages/core/src'
+import { simpleAnimator } from '../packages/core/src'
 import { input, output } from './fixture'
 
 describe('animator', () => {
   it('generate', () => {
-    const delta = diffString(input, output)
-    const patches = calculatePatches(delta)
-    const animator = createAnimator(input, patches)
+    const animator = simpleAnimator(input, output)
     expect([...animator]).toMatchSnapshot('animator')
   })
 })
